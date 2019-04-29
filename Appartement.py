36801ece7495cca7ec1ed008037d4980d3797f5a
duwenzhen <wenzhen.du@gmail.com>
2019-04-28 22:12:41 -0400

    init commit

---
+++ Appartement.py
@@ -0,0 +1,39 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+
+import glob
+
+
+path = r"."
+df = pd.read_csv(path + "appartement.csv", index_col=None, header=0, sep='|')
+df["Date mutation"] = pd.to_datetime(df["Date mutation"],format="%Y/%m/%d")
+df["YearMonth"] = df["Date mutation"].dt.strftime('%Y%m')
+tmp = df.loc[df["Code departement"] == 75].groupby(['YearMonth'])['PrixM2'].count()
+
+
+plt.figure(figsize=(8,6))
+plt.scatter(range(df.shape[0]), np.sort(np.log(df.PrixM2.values)))
+plt.xlabel('index', fontsize=12)
+plt.ylabel('PrixM2', fontsize=12)
+plt.show()
+
+plt.figure(figsize=(12,6))
+sns.barplot(tmp.index, tmp, alpha=0.8, color=color[3])
+plt.xticks(rotation='vertical')
+plt.xlabel('Price', fontsize=12)
+plt.ylabel('Date', fontsize=12)
+plt.show()
+
+
+tmp = df.loc[df["Code postal"] == 75016].groupby(['YearMonth'])['PrixM2'].median()
+
+
+plt.figure(figsize=(12,6))
+sns.barplot(tmp.index, tmp, alpha=0.8, color=color[3])
+plt.xticks(rotation='vertical')
+plt.xlabel('Price', fontsize=12)
+plt.ylabel('Date', fontsize=12)
+plt.show()
---
+++ Main.py
@@ -0,0 +1,42 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import glob
+
+
+path = r"."
+from subprocess import check_output
+print(check_output(["ls", path]).decode("utf8"))
+all_files = glob.glob(path + "/valeursfoncieres*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0, sep='|')
+    df = df.iloc[:, 7:].fillna(0)
+    print(df.head())
+    print(df.shape)
+    li.append(df)
+df = pd.concat(li, axis=0, ignore_index=True)
+print(df.shape)
+df = df.loc[df["Code type local"] == 2]
+df["Surface Carrez du 1er lot"] = df["Surface Carrez du 1er lot"].str.replace(',','.').astype(float)
+df["Surface Carrez du 2eme lot"] = df["Surface Carrez du 2eme lot"].str.replace(',','.').astype(float)
+df["Surface Carrez du 3eme lot"] = df["Surface Carrez du 3eme lot"].str.replace(',','.').astype(float)
+df["Surface Carrez du 4eme lot"] = df["Surface Carrez du 4eme lot"].str.replace(',','.').astype(float)
+df["Surface Carrez du 5eme lot"] = df["Surface Carrez du 5eme lot"].str.replace(',','.').astype(float)
+df = df.fillna(0)
+df = df.loc[(df["Surface Carrez du 1er lot"] != 0 )| (df["Surface Carrez du 2eme lot"] != 0)|(df["Surface Carrez du 3eme lot"] != 0)|(df["Surface Carrez du 4eme lot"] != 0)|(df["Surface Carrez du 5eme lot"] != 0)]
+print(df["Code type local"].head())
+print(df.shape)
+df["ID"] = df["Code departement"].map(str) + df["Code commune"].map(str) + df["Prefixe de section"].map(str) + df["Section"].map(str) + df["No plan"].map(str) + df["No disposition"].map(str) + df["Type local"].map(str) + df["Date mutation"].map(str)
+print(df.shape)
+print(df["ID"].nunique())
+df["SurfaceCarrez"] = df["Surface Carrez du 1er lot"].astype(float) + df["Surface Carrez du 2eme lot"].astype(float)+df["Surface Carrez du 3eme lot"].astype(float) +df["Surface Carrez du 4eme lot"].astype(float) +df["Surface Carrez du 5eme lot"].astype(float)
+df["Valeur fonciere"] = df["Valeur fonciere"].str.replace(',', '.').astype(float)
+df = df.fillna(0)
+df = df.loc[df["Valeur fonciere"] > 0]
+df["PrixM2"] = df["Valeur fonciere"]  / df["SurfaceCarrez"]
+df["Date mutation"] = pd.to_datetime(df["Date mutation"],format="%d/%m/%Y")
+df.to_csv(r"./appartement.csv", sep='|')
